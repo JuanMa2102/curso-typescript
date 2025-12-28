18713f5c10ac9bbffdcf455612127d9c075f3196
Manuel Martínez <manuelmartinez199802@gmail.com>
2025-12-28 00:22:19 -0600

    uso de exports

---
+++ src/data/powers.ts
@@ -0,0 +1,14 @@
+interface Power {
+    id: number;
+    desc: string;
+}
+
+const powers: Power[] = [
+    { id: 1, desc: 'Superhuman strength' },
+    { id: 2, desc: 'Superhuman speed' },
+    { id: 3, desc: 'Superhuman agility' },
+    { id: 4, desc: 'Superhuman durability' },
+    { id: 5, desc: 'Superhuman intelligence' }
+];
+
+export default powers;
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,12 @@
-import * as HeroClasses from './clases/Hero';
+import { Hero } from './clases/Hero';
+// import * as HeroClasses from './clases/Hero';
+// import powers from './data/powers';
 
-console.log('Hola Mundo!!');
+// console.log('Hola Mundo!!');
 
-const hero = new HeroClasses.Hero('Spiderman', 1, 25);
-console.log(hero);
+// const hero = new HeroClasses.Hero('Spiderman', 1, 25);
+// console.log(hero);
+// console.log(powers[hero.powerId - 1].desc);
+
+const ironman = new Hero('Ironman', 5, 40);
+console.log(ironman.power);
