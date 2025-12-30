790668cb1664f3a10394c4192434c4c187088e67
Manuel Martínez <manuelmartinez199802@gmail.com>
2025-12-30 03:40:40 -0600

    funciones genericas

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,15 @@
 import { printObject, genericFunction } from "./generics/generics";
+import { Hero } from "./interfaces/hero";
 
 printObject({ name: "Juanma", age: 23 });
 
-console.log( genericFunction("Hello World").toUpperCase() );
-console.log( genericFunction(23).toFixed(2) );
-console.log( genericFunction( new Date() ).getDay() );
+// console.log( genericFunction("Hello World").toUpperCase() );
+// console.log( genericFunction(23).toFixed(2) );
+// console.log( genericFunction( new Date() ).getDay() );
+const deadpool = {
+    name: "Deadpool",
+    realName: "Wade Winston",
+    dangerLevel: 130
+};
+
+console.log( genericFunction<Hero>(deadpool).name );
