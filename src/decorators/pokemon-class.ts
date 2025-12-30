245288d57d812172f95966d007077d7d77be4b2b
Manuel Martínez <manuelmartinez199802@gmail.com>
2025-12-30 04:52:13 -0600

    decorador con methodos

--- src/decorators/pokemon-class.ts
+++ src/decorators/pokemon-class.ts
@@ -15,9 +15,29 @@ const blockPrototype = function( constructor: Function ) {
     Object.seal( constructor.prototype );
 }
 
+// factory decorator
+function ChekkValidPokemonId():Function {
+    return function( target: any, propertyKey: string, descriptor: PropertyDescriptor ) {
+        const originalMethod = descriptor.value;
+        descriptor.value = ( id: number ) => {
+            if ( id < 1 || id > 800 ) {
+                return console.error('El id del pokemon debe estar entre 1 y 800');
+            } else {
+                return originalMethod( id );
+            }
+        }
+    }
+}
+
+
 @blockPrototype
 @printToConsoleConditional( true )
 export class Pokemon {
     public publicApi: string = 'https://pokeapi.co/';
     constructor(public id: number, public name: string) {}
+
+    @ChekkValidPokemonId()
+    savePokemonToDB( id: number ){
+        console.log(`Saving to DB ${ id }`);
+    }
 }
